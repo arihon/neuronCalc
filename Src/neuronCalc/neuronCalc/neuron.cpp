ff740f73e4084258d35dd3d64b21dca2832eac4e
arihon <arihon22@gmail.com>
2016-09-18 22:08:55 +0900

    class:neuronを更新

--- Src/neuronCalc/neuronCalc/neuron.cpp
+++ Src/neuronCalc/neuronCalc/neuron.cpp
@@ -1,12 +1,43 @@
-#include "stdafx.h"
 #include "neuron.h"
+#include<math.h>
 
 
-neuron::neuron()
+neuron::neuron(int inputLayerSize, double sigmoidGain)
 {
+	/*コンストラクタ*/
+	size = inputLayerSize;
+	gain = sigmoidGain;
+	sum = 0.0;
+	q = 0.0;
+	for (int i = 0; i < size; i++) {
+		weight[i] = 1.0;
+	}
 }
 
 
 neuron::~neuron()
 {
+}
+/*private*/
+double neuron::sigmoid(double gain, double x)
+{
+	return 1.0 / (1.0 + exp(-gain * x));
+}
+/*public*/
+void neuron::reset() {
+	sum = 0;
+	return;
+}
+void neuron::input(int inputFrom, double u)
+{
+	sum += u * weight[inputFrom];
+	return;
+}
+void neuron::calc() {
+	q = sigmoid(gain, sum);
+	return;
+}
+double neuron::output()
+{
+	return q;
 }
--- Src/neuronCalc/neuronCalc/neuron.h
+++ Src/neuronCalc/neuronCalc/neuron.h
@@ -2,7 +2,20 @@
 class neuron
 {
 public:
-	neuron();
+	neuron(int inputLayerSize, double sigmoidGain);
 	~neuron();
-};
+private:
+	int size;
+	double gain;
+	int sum;
+	double weight[1024];
+	double q;
 
+	double sigmoid(double gain, double x);
+
+public:
+	void reset();
+	void input(int inputFrom, double u);
+	void calc();
+	double output();
+};
